8d28a45b1cd805cc62638f9fa202e56681e9e3e4
Marcel Teixeira <marceltex@gmail.com>
2016-07-01 14:56:44 +0200

    Add button which launches a file chooser when clicked

--- app/src/main/java/com/marcel/example/aes_256poc/AES256Activity.java
+++ app/src/main/java/com/marcel/example/aes_256poc/AES256Activity.java
@@ -1,13 +1,28 @@
 package com.marcel.example.aes_256poc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AES256Activity extends AppCompatActivity {
+
+    Button selectFileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aes256);
+
+        selectFileBtn = (Button) findViewById(R.id.button_select_file);
+
+        selectFileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+                startActivityForResult(intent, 100);
+            }
+        });
     }
 }
