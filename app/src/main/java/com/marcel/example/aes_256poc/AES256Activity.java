a9794911ba621a650d3107e1db9b72fd0ea8c1c5
Marcel Teixeira <marceltex@gmail.com>
2016-07-04 10:00:11 +0200

    Add method to process result from file chooser activity

--- app/src/main/java/com/marcel/example/aes_256poc/AES256Activity.java
+++ app/src/main/java/com/marcel/example/aes_256poc/AES256Activity.java
@@ -1,14 +1,18 @@
 package com.marcel.example.aes_256poc;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class AES256Activity extends AppCompatActivity {
 
     private static final int READ_REQUEST_CODE = 42;
+    private static final String TAG = "AES256_Activity";
 
     Button selectFileBtn;
 
@@ -36,5 +40,16 @@ public class AES256Activity extends AppCompatActivity {
                 startActivityForResult(intent, READ_REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i(TAG, "Uri: " + uri.toString());
+            }
+        }
     }
 }
