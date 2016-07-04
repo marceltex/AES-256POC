63d759c11a48c2b5d0b55b70250c49c3c349ade3
Marcel Teixeira <marceltex@gmail.com>
2016-07-04 09:42:48 +0200

    Fix bug that was causing app to crash when select files button is clicked

--- app/src/main/java/com/marcel/example/aes_256poc/AES256Activity.java
+++ app/src/main/java/com/marcel/example/aes_256poc/AES256Activity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 
 public class AES256Activity extends AppCompatActivity {
 
+    private static final int READ_REQUEST_CODE = 42;
+
     Button selectFileBtn;
 
     @Override
@@ -20,8 +22,18 @@ public class AES256Activity extends AppCompatActivity {
         selectFileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
-                startActivityForResult(intent, 100);
+                // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+                // browser.
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+                // Filter to only show results that can be "opened", such as a
+                // file (as opposed to a list of contacts or timezones)
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                // Search for all documents available via installed service providers
+                intent.setType("*/*");
+
+                startActivityForResult(intent, READ_REQUEST_CODE);
             }
         });
     }
